4afb82dfb83ad285cecef37dab36b0b6b58124f5
RUTAHIGWA EMMANUEL NOEL <77039776+EmmanuelNoel01@users.noreply.github.com>
2023-07-04 12:24:37 -0700

    new

--- lib/user/login.dart
+++ lib/user/login.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: use_build_context_synchronously, prefer_const_constructors
-import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import 'navbar.dart';
 import 'signup.dart';
@@ -19,9 +18,6 @@ class _LogInState extends State<LogIn> {
   final passController = TextEditingController();
   bool passToggle = true;
 
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,66 +34,108 @@ class _LogInState extends State<LogIn> {
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                  Image.asset(
-                    "assets/welcome.PNG",
-                    height: 150,
-                    width: 200,
+                Image.asset(
+                  "assets/welcome.PNG",
+                  height: 150,
+                  width: 200,
+                ),
+                const SizedBox(height: 50,),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    labelText: "Email",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
                   ),
-                  const SizedBox(height: 50,),
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                      labelText: "Email",
-                      border: OutlineInputBorder(),
-                      prefixIcon: Icon(Icons.email),
-                    ),
-                    validator: (value){
-                      bool emailValid = RegExp(
-                          r"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value!);
-                      if(value.isEmpty){
-                        return "Enter Email";
-                      }
-                      else if(!emailValid){
-                        return "Enter Valid Email";
-                      }
+                  validator: (value){
+                    bool emailValid = RegExp(
+                        r"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value!);
+                    if(value.isEmpty){
+                      return "Enter Email";
                     }
-                  ),
-                  const SizedBox(height: 20,),
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    controller: passController,
-                    obscureText: passToggle,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      border: const OutlineInputBorder(),
-                      prefixIcon: const Icon(Icons.lock),
-                      suffixIcon: InkWell(
-                        onTap: (){
-                          setState(() {
-                            passToggle = !passToggle;
-                          });
-                        },
-                        child: Icon(passToggle ? Icons.visibility : Icons.visibility_off),
-                      ),
-                    ),
-                    validator: (value){
-                      if(value!.isEmpty){
-                        return "Enter Password";
-                      }
-                      else if(passController.text.length < 8){
-                        return "Enter Password of more than 8 charaters";
-                      }
+                    else if(!emailValid){
+                      return "Enter Valid Email";
                     }
+                  }
+                ),
+                const SizedBox(height: 20,),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: passController,
+                  obscureText: passToggle,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    border: const OutlineInputBorder(),
+                    prefixIcon: const Icon(Icons.lock),
+                    suffixIcon: InkWell(
+                      onTap: (){
+                        setState(() {
+                          passToggle = !passToggle;
+                        });
+                      },
+                      child: Icon(passToggle ? Icons.visibility : Icons.visibility_off),
+                    ),
                   ),
-                  const SizedBox( height: 60,),
-                  InkWell(
-                  onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => NavBar()),
-                  );
-                },
+                  validator: (value){
+                    if(value!.isEmpty){
+                      return "Enter Password";
+                    }
+                    else if(passController.text.length < 8){
+                      return "Enter Password of more than 8 characters";
+                    }
+                  }
+                ),
+                const SizedBox( height: 60,),
+                InkWell(
+                  onTap: () async {
+                    final email = emailController.text;
+                    final password = passController.text;
+
+                    final url = 'http://192.168.30.34/apis/login.php';
+                    final response = await http.post(Uri.parse(url), body: {
+                      'email': email,
+                      'password': password,
+                    });
+
+                    if (response.statusCode == 200) {
+                      final result = json.decode(response.body);
+                      if (result['status'] == 'success') {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => NavBar()),
+                        );
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: Text('Login Failed'),
+                            content: Text('Invalid email or password.'),
+                            actions: [
+                              TextButton(
+                                child: Text('OK'),
+                                onPressed: () => Navigator.pop(context),
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: Text('Error'),
+                          content: Text('Failed to connect to the server.'),
+                          actions: [
+                            TextButton(
+                              child: Text('OK'),
+                              onPressed: () => Navigator.pop(context),
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+                  },
                   child: Container(
                     height: 50,
                     decoration: BoxDecoration(
@@ -112,40 +150,42 @@ class _LogInState extends State<LogIn> {
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
                         ),
-                        ),
                       ),
                     ),
                   ),
-                  const SizedBox( height: 20,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Don't have an account?",
+                ),
+                const SizedBox( height: 20,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Don't have an account?",
+                      style: TextStyle(
+                        fontSize: 17,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: (){
+                        _navigateToNextScreen(context);
+                      },
+                      child: const Text(
+                        "Sign Up",
                         style: TextStyle(
-                          fontSize: 17,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      TextButton(
-                          onPressed: (){
-                            _navigateToNextScreen(context);
-                          },
-                          child: const Text(
-                            "Sign Up",
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                          ))
-                    ],
-                  ),
-                ],
-              ),
+                    )
+                  ],
+                ),
+              ],
             ),
+          ),
         ),
       ),
     );
   }
+
   void _navigateToNextScreen(BuildContext context) {
     Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignUp()));
   }
